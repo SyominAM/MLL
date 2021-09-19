c3b62f5e6d5524c8c83fa94a77d78e3cebe50be7
Alexander <seomin.aleksaner2015@yandex.ru>
2021-09-19 14:10:08 +0300

    11245

--- web.py
+++ web.py
@@ -20,5 +20,5 @@ def index():
         return render_template('index.html', title="Это: " + response.text)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__' :
     app.run(port=8080, host='127.0.0.1')
